22ee35d2a7148d08058265c267abc4ef828cf7e3
kevin flynn <flynny85@gmail.com>
2019-05-13 14:30:40 +0100

    updating app-version visual.

--- front/src/components/c_app_footer.vue
+++ front/src/components/c_app_footer.vue
@@ -4,41 +4,38 @@
 		class="anim-6 colour-bg-inv"> 
 
 		<div 
-			class="footer-content center-auto-h width-85 anim-6">
+			class="footer-content center-auto-h">
 
+			<p class="colour-fill-bg text text-center">Made with 
 
-			<div class="text text-small1 text-center">
-				<p class="colour-fill-bg">Made with 
+				<span>
 
-					<span>
+					<svg 
+						class="icon" 
+						viewBox="0 0 907 828" 
+						version="1.1" 
+						xmlns="http://www.w3.org/2000/svg" 
+						xmlns:xlink="http://www.w3.org/1999/xlink" 
+						xml:space="preserve" 
+						xmlns:serif="http://www.serif.com/">
 
-						<svg 
-							class="icon" 
-							viewBox="0 0 907 828" 
-							version="1.1" 
-							xmlns="http://www.w3.org/2000/svg" 
-							xmlns:xlink="http://www.w3.org/1999/xlink" 
-							xml:space="preserve" 
-							xmlns:serif="http://www.serif.com/">
+						<path class="" d="M730.084,312.85c4.226,113.133 -40.828,236.381 -79.883,320.702c-29.137,63.033 -92.165,103.467 -161.607,103.675c-58.148,0.184 -129.832,0.398 -188.066,0.573c-68.886,0.206 -131.752,-39.215 -161.569,-101.314c-43.597,-90.793 -96.522,-224.663 -88.059,-325.713"/>
+						<path class="" d="M710,410.13l137.329,0c0,0 27.947,188.444 -226.001,233.406"/>
 
-							<path class="" d="M730.084,312.85c4.226,113.133 -40.828,236.381 -79.883,320.702c-29.137,63.033 -92.165,103.467 -161.607,103.675c-58.148,0.184 -129.832,0.398 -188.066,0.573c-68.886,0.206 -131.752,-39.215 -161.569,-101.314c-43.597,-90.793 -96.522,-224.663 -88.059,-325.713"/>
-							<path class="" d="M710,410.13l137.329,0c0,0 27.947,188.444 -226.001,233.406"/>
+					</svg>
 
-						</svg>
+				</span>
+				by 
 
-					</span> 
-					by 
+				<a class="colour-fill-accent" target="_blank" href="https://github.com/k-flynn-webdev"> k-flynn-webdev </a>
 
-					<a class="colour-fill-accent" target="_blank" href="https://github.com/k-flynn-webdev"> k-flynn-webdev </a>
-
-					<p class="text app-version">
-						v: {{ version }}
-					</p>
-
-				</p>
-			</div>
+			</p>
 
 		</div>
+
+		<p class="colour-fill-bg text app-version">
+			v: {{ version }}
+		</p>		
 
 	</footer>
 
@@ -70,34 +67,27 @@ export default {
 <style scoped >
 
 	footer {
-		/*position: absolute;*/
-		/*bottom: 0;*/
-		/*left: 0;*/
+		margin-top: 2rem;
 		width: 100%;
-		/*z-index: 1;*/
-		/*overflow: hidden;*/
-		/*opacity: 0;*/
-		/*pointer-events: none;*/
+		max-width: 35rem;
+		margin: 0 auto;
 	}
 
 	.footer-content {
-		/*pointer-events: visible;*/
-		/*opacity: 0;*/
-		/*transform: translateY(4rem);*/
+		width: 18rem;
+		display: inline-block;
+		margin: 0;
+		padding: 0;
 	}
-	
 
 	.app-version {
-		font-size: .85rem;
-		position: absolute;
-		right: 0;
-		bottom: 0;
-		margin: 0 .7rem;
+		font-size: .8rem;
+		display: block;
+		text-align: right;
+		margin: .5rem;
+		margin-top: .2rem;
 	}
 
-	.footer-content div{
-		margin: .25rem;
-	}
 	.icon {
 		transform: translateY(.15rem);
 		width: 1.5rem;
